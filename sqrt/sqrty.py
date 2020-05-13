b69790eb3ebc3240173d060bbf89dae1a4e8218f
John Dalton <jldalton@containerstore.com>
2020-05-12 23:43:48 -0500

    clean up

--- sqrt/sqrty.py
+++ sqrt/sqrty.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python
 
 """
-Calculates the square root of a number to N digits.
-Using yield.
+A generator that calculates the square root of a number to N digits.
 
 Reference: https://en.wikipedia.org/wiki/Methods_of_computing_square_roots
 
@@ -14,13 +13,6 @@ import datetime
 
 DECIMAL_POINT = '.'
 
-def odd_length(lst):
-    return 1 == len(lst) % 2
-
-def ceil_div(a, b):
-    # integer division with ceiling
-    # ref: https://python-history.blogspot.com/2010/08/why-pythons-integer-division-floors.html
-    return -(a // -b)
 
 class RootCalculator:
     def __init__(self):
@@ -29,7 +21,18 @@ class RootCalculator:
         self.remainder = 0
         self.result = 0
 
-    def group_into_pairs(self,n):
+    @staticmethod
+    def odd_length(lst):
+        return 1 == len(lst) % 2
+
+    @staticmethod
+    def ceil_div(a, b):
+        # integer division with ceiling
+        # ref: https://python-history.blogspot.com/2010/08/why-pythons-integer-division-floors.html
+        return -(a // -b)
+
+    @staticmethod
+    def group_into_pairs(n):
         """
         Given: n, the number to compute the square root of
         Return: a list of the digits of n, with decimal point, and leading or trailing zeroes, if needed.
@@ -52,9 +55,9 @@ class RootCalculator:
         p = lst.index(DECIMAL_POINT)
         before = lst[0:p]
         after = lst[p+1:]
-        if odd_length(before):
+        if RootCalculator.odd_length(before):
             lst.insert(0, str(0))
-        if odd_length(after):
+        if RootCalculator.odd_length(after):
             lst.append(str(0))
         return lst
 
@@ -63,64 +66,61 @@ class RootCalculator:
         Note: the first time, current will be the first pair itself, since
         the remainder is initialized to 0. i.e. sqrt(153) ==> current = 01 = 1
         """
-        if not self.pairs: # once lst is empty, keep bringing down 00
+        if not self.pairs:  # once lst is empty, keep bringing down 00
             addend = 0
         else:
             addend = int("%s%s" % (self.pairs[0], self.pairs[1]))
         self.pairs = self.pairs[2:]
         return self.remainder * 100 + addend
 
-
-    def find_next_digit(self):
+    def next_digit(self):
         """
         Find p, y, and x as follows:
 
         Let c = current
         Let p = partial (the part of the root found so far, ignoring the decimal point)
         The first time through, p will be 0.
+
+        Let x = the next digit
         Let y = x * (20p + x) ... Note: 20p + x is simply twice p with the digit x appended to the right
 
-        Determine the greatest digit x such that x *(20p + x) <= c
+        Note: the next digit is the greatest digit x such that x * (20p + x) <= c
 
-        Try each digit from 9 to 0 until one works.
-        Note: instead of brute force of starting with 9, we could take a short cut by starting with
-        a value near c / (20p)
+        Try each digit from 9 to 0 until one works;
+        but to optimize, instead of the brute force method of starting with 9,
+        starting with a value near c / (20p) (rounding up)
         """
 
-        MAX_DIGIT = 9
-        MULTIPLIER = 20 # shift left in base ten, multiplied by 2 since doing square roots
-
-        # add decimal point to answer at correct spot
+        # add decimal point to answer at the correct spot
         if self.pairs and self.pairs[0] == DECIMAL_POINT:
             self.pairs = self.pairs[1:]
             return DECIMAL_POINT
 
+        # short circuit for rational solutions
         if self.remainder == 0 and not self.pairs:
-            # short circuit for rational answers
             return -1
 
-        self.current = self.current_from_next_pair()
+        current = self.current_from_next_pair()
 
-        twenty_p = MULTIPLIER * self.partial_root
-        upper_digit = MAX_DIGIT
+        twenty_p = 20 * self.partial_root
+        upper_digit = 9
         if twenty_p > 0:
-            # short cut
-            upper_digit = ceil_div(self.current, twenty_p + MAX_DIGIT)
+            upper_digit = RootCalculator.ceil_div(current, twenty_p + 9)  # short cut
 
         for x in range(upper_digit, -1, -1):
             y = x * (twenty_p + x)
-            if y <= self.current:
-                self.remainder = self.current - y
+            if y <= current:
+                self.remainder = current - y
                 self.partial_root = self.partial_root*10 + x
                 return x
 
-        print("Unexpected state in find_next_digit")
+        print("Unexpected state in next_digit")
         sys.exit(1)
 
     def sqrt(self, n):
         self.pairs = self.group_into_pairs(n)
         while True:
-            yield self.find_next_digit()
+            yield self.next_digit()
 
 
 def get_input():
@@ -144,16 +144,7 @@ def get_input():
     return n, size, raw
 
 
-def output(lst):
-    print("".join(lst))
-
-
-def output_with_breaks(lst, line_size=80):
-    while lst:
-        output(lst[:line_size])
-        lst = lst[line_size:]
-
-def show_answer(digits, size, raw):
+def show_result(digits, size, raw):
     count = 0
     line_count = 0
     progress = None
@@ -165,7 +156,7 @@ def show_answer(digits, size, raw):
         count += 1
         if not raw:
             line_count += 1
-            if count % (size // 10) == 0:
+            if size >= 100_000 and count % (size // 10) == 0:
                 progress = f"    {100*(count / size)}% {datetime.datetime.now().time()}"
             if line_count == 80:
                 line_count = 0
@@ -180,6 +171,7 @@ def show_answer(digits, size, raw):
     print()
     print()
 
+
 def main():
     n, size, raw = get_input()
     print()
@@ -189,11 +181,13 @@ def main():
     began = datetime.datetime.now().time()
 
     digits = RootCalculator().sqrt(n)
-    show_answer(digits, size, raw)
+    show_result(digits, size, raw)
 
     ended = datetime.datetime.now().time()
+
     print(f"Began at {began}")
     print(f"Ended at {ended}")
+
 
 if __name__ == "__main__":
     main()
